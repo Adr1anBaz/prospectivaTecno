4c4653598134d71d89c36e7b42717ded10d10843
Sebitas bebé <chuchoenguilo160@gmail.com>
2026-06-15 14:14:16 -0600

    imagen

--- proyecto-final/slides/src/slides/ObjetivoSlide.jsx
+++ proyecto-final/slides/src/slides/ObjetivoSlide.jsx
@@ -62,7 +62,7 @@ export default function ObjetivoSlide() {
 
       <motion.div
         initial={{ opacity: 0, scale: 0.95 }}
-        animate={{ opacity: 1, scale: 1 }}
+        animate={{ opacity: 1, scale: 0.5 }}
         transition={{ delay: 0.85, duration: 0.5 }}
         style={{
           marginTop: 48,
